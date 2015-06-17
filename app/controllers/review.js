37a3c8f1a6a133db42f00351a227be4018648a2e
Kevin Chen & Ian Bevel <student@epicodus.com>
2015-06-17 14:48:00 -0700

    allow user to edit and delete reviews

--- app/controllers/review.js
+++ app/controllers/review.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['game'],
+  isEditing: false,
+  actions: {
+    editReview: function() {
+      this.set('isEditing', true);
+    },
+    updateReview: function() {
+      this.set('isEditing', false);
+      this.get('model').save();
+    },
+    deleteReview: function() {
+      var review = this.get('model');
+      var game = this.get('controllers.game.model');
+      game.get('reviews').removeObject(review);
+      game.save();
+      review.destroyRecord();
+
+      this.transitionToRoute('game', game.id);
+    }
+  }
 });
